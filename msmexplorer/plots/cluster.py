e48eeab11696c2fcf90cad92ad0a72e617b01e56
Carlos Hernández <cxhernandez@users.noreply.github.com>
2016-09-21 11:31:06 -0700

    Update cluster.py

--- msmexplorer/plots/cluster.py
+++ msmexplorer/plots/cluster.py
@@ -134,6 +134,5 @@ def plot_voronoi(kmeans, ax=None, obs=(0, 1), cluster_centers=True,
     if we_made_ax:
         ax.set_xlim((vor.min_bound[0] - 0.1, vor.max_bound[0] + 0.1))
         ax.set_ylim((vor.min_bound[1] - 0.1, vor.max_bound[1] + 0.1))
-        ax.axis('equal')
 
     return ax

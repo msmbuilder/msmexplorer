480768a66599fbd5287b67411b5f012414b3a6e2
msultan <msultan@stanford.edu>
2017-03-02 16:05:42 -0800

    minor free energy fix

--- msmexplorer/plots/projection.py
+++ msmexplorer/plots/projection.py
@@ -130,7 +130,8 @@ def plot_free_energy(data, ax=None, obs=0, temperature=300., n_samples=None,
 
         ax.plot(X, Z - Z.min(), color=color)
 
-        ax.fill_between(X, Z - Z.min(), Z.max() - Z.min(),
+        if shade:
+            ax.fill_between(X, Z - Z.min(), Z.max() - Z.min(),
                         facecolor=color, alpha=alpha)
 
     elif prune.shape[1] == 2:

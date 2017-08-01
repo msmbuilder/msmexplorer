d83ad4baf502c3840e03899e61f7f7d176c2921b
Carlos Hernandez <cxh@stanford.edu>
2017-08-01 12:57:06 -0700

    add example

---
+++ examples/plot_stackdist.py
@@ -0,0 +1,40 @@
+"""
+Chord Diagram
+=============
+"""
+from msmbuilder.example_datasets import FsPeptide
+from msmbuilder.featurizer import DihedralFeaturizer
+from msmbuilder.decomposition import tICA
+from msmbuilder.cluster import MiniBatchKMeans
+from msmbuilder.msm import MarkovStateModel
+
+import numpy as np
+
+import msmexplorer as msme
+
+rs = np.random.RandomState(42)
+
+# Load Fs Peptide Data
+trajs = FsPeptide().get().trajectories
+
+# Extract Backbone Dihedrals
+featurizer = DihedralFeaturizer(types=['chi1'])
+diheds = featurizer.fit_transform(trajs)
+
+# Perform Dimensionality Reduction
+tica_model = tICA(lag_time=2, n_components=2)
+tica_trajs = tica_model.fit_transform(diheds)
+
+# Perform Clustering
+clusterer = MiniBatchKMeans(n_clusters=12, random_state=rs)
+clustered_trajs = clusterer.fit_transform(tica_trajs)
+
+# Construct MSM
+msm = MarkovStateModel(lag_time=2)
+assignments = msm.fit_transform(clustered_trajs)
+
+# Plot Stacked Distributions
+a = np.concatenate(assignments, axis=0)
+d = np.concatenate(diheds, axis=0)
+
+msme.plot_stackdist([d[a == i][:, ::2] for i in [2, 5, 0]])

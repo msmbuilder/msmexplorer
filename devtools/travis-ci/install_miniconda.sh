40202608fcbbdf91b7d75164e2bdb27dfbf71a96
Carlos Hernández <cxhernandez@users.noreply.github.com>
2017-08-03 12:48:06 -0700

    Update install_miniconda.sh

--- devtools/travis-ci/install_miniconda.sh
+++ devtools/travis-ci/install_miniconda.sh
@@ -2,10 +2,16 @@
 MINICONDA=Miniconda3-latest-Linux-x86_64.sh
 MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget https://repo.continuum.io/miniconda/$MINICONDA
+
+if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+    alias md5sum='md5 -r'
+fi
+
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
 fi
+
 bash $MINICONDA -b
 rm -f $MINICONDA
 

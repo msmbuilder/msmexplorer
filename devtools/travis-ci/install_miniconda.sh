7178a31a94a30cf87d4ef46ffc13ec13e3df29d7
Carlos Hernández <cxhernandez@users.noreply.github.com>
2017-08-03 13:54:39 -0700

    Update install_miniconda.sh

--- devtools/travis-ci/install_miniconda.sh
+++ devtools/travis-ci/install_miniconda.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
     MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh
 else
     MINICONDA=Miniconda3-latest-Linux-x86_64.sh
-if
+fi
 
 MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget https://repo.continuum.io/miniconda/$MINICONDA

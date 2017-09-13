d62d81fb9cbdc3ceba97fb71f6f21b4f5c54630a
Juan Eiros <jeirosz@gmail.com>
2017-09-13 16:37:10 +0100

    Add example of trace2d
    On top of 2d projection of free energy for clarity

---
+++ examples/plot_trace2d.py
@@ -0,0 +1,46 @@
+"""
+Two dimensional trace plot
+===============
+"""
+from msmbuilder.example_datasets import FsPeptide
+from msmbuilder.featurizer import DihedralFeaturizer
+from msmbuilder.decomposition import tICA
+from msmbuilder.cluster import MiniBatchKMeans
+from msmbuilder.msm import MarkovStateModel
+
+import numpy as np
+
+import msmexplorer as msme
+
+rs = np.random.RandomState(42)
+
+# Load Fs Peptide Data
+trajs = FsPeptide().get().trajectories
+
+# Extract Backbone Dihedrals
+featurizer = DihedralFeaturizer(types=['phi', 'psi'])
+diheds = featurizer.fit_transform(trajs)
+
+# Perform Dimensionality Reduction
+tica_model = tICA(lag_time=2, n_components=2)
+tica_trajs = tica_model.fit_transform(diheds)
+
+# Plot free 2D free energy (optional)
+txx = np.concatenate(tica_trajs, axis=0)
+ax = msme.plot_free_energy(
+    txx, obs=(0, 1), n_samples=100000,
+    random_state=rs,
+    shade=True,
+    clabel=True,
+    clabel_kwargs={'fmt': '%.1f'},
+    cbar=True,
+    cbar_kwargs={'format': '%.1f', 'label': 'Free energy (kcal/mol)'}
+)
+# Now plot the first single trajectory on top of it to inspect it's movement
+msme.plot_trace2d(
+    data=tica_trajs[0], ts=.2, ax=ax,
+    scatter_kwargs={'s': 2},
+    cbar_kwargs={'format': '%.1f', 'label': 'Time (ns)',
+                 'orientation': 'horizontal'},
+    xlabel='tIC 1', ylabel='tIC 2'
+)
